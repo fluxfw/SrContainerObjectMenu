7bc0b718baf2ef92c47922d22e9a0437cef642b1
Florian Wyss <fw@studer-raimann.ch>
2020-04-14 14:04:22 +0200

    Fix

--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -12,7 +12,7 @@ class ComposerStaticInite59bf074d7a0c4f325ca3aaedb8a0e7f
             'srag\\RemovePluginDataConfirm\\SrContainerObjectMenu\\' => 51,
             'srag\\Plugins\\SrContainerObjectMenu\\' => 35,
             'srag\\LibrariesNamespaceChanger\\' => 31,
-            'srag\\DataTableUI\\' => 17,
+            'srag\\DataTableUI\\SrContainerObjectMenu\\' => 39,
             'srag\\DIC\\SrContainerObjectMenu\\' => 31,
             'srag\\CustomInputGUIs\\' => 21,
         ),
@@ -31,7 +31,7 @@ class ComposerStaticInite59bf074d7a0c4f325ca3aaedb8a0e7f
         array (
             0 => __DIR__ . '/..' . '/srag/librariesnamespacechanger/src',
         ),
-        'srag\\DataTableUI\\' => 
+        'srag\\DataTableUI\\SrContainerObjectMenu\\' => 
         array (
             0 => __DIR__ . '/..' . '/srag/datatable/src',
         ),
