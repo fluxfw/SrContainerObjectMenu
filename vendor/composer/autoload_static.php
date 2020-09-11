b734dd39e8040398b115a24dfc1c02e9f2b3cb55
Florian Wyss <fw@studer-raimann.ch>
2020-09-11 13:06:56 +0200

    Fix

--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -13,17 +13,17 @@ class ComposerStaticInite59bf074d7a0c4f325ca3aaedb8a0e7f
     public static $prefixLengthsPsr4 = array (
         's' => 
         array (
-            'srag\\RemovePluginDataConfirm\\' => 29,
+            'srag\\RemovePluginDataConfirm\\SrContainerObjectMenu\\' => 51,
             'srag\\Plugins\\SrContainerObjectMenu\\' => 35,
             'srag\\LibrariesNamespaceChanger\\' => 31,
             'srag\\DataTableUI\\SrContainerObjectMenu\\' => 39,
-            'srag\\DIC\\' => 9,
-            'srag\\CustomInputGUIs\\' => 21,
+            'srag\\DIC\\SrContainerObjectMenu\\' => 31,
+            'srag\\CustomInputGUIs\\SrContainerObjectMenu\\' => 43,
         ),
     );
 
     public static $prefixDirsPsr4 = array (
-        'srag\\RemovePluginDataConfirm\\' => 
+        'srag\\RemovePluginDataConfirm\\SrContainerObjectMenu\\' => 
         array (
             0 => __DIR__ . '/..' . '/srag/removeplugindataconfirm/src',
         ),
@@ -39,11 +39,11 @@ class ComposerStaticInite59bf074d7a0c4f325ca3aaedb8a0e7f
         array (
             0 => __DIR__ . '/..' . '/srag/datatable/src',
         ),
-        'srag\\DIC\\' => 
+        'srag\\DIC\\SrContainerObjectMenu\\' => 
         array (
             0 => __DIR__ . '/..' . '/srag/dic/src',
         ),
-        'srag\\CustomInputGUIs\\' => 
+        'srag\\CustomInputGUIs\\SrContainerObjectMenu\\' => 
         array (
             0 => __DIR__ . '/..' . '/srag/custominputguis/src',
         ),
